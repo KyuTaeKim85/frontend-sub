66c373a4ac36badf7d062241738dd14478fd4ee2
kkt <k31726@nate.com>
2025-12-07 15:41:53 +0900

    Revert "test"
    This reverts commit e369f056d8dd37bcd7bf0405a63ee7dbdfd05fb4.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Main1 from './layout/Main';
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('jwt_token'));
-
-  const setAuthToken = (newToken) => {
-    if (newToken) {
-      localStorage.setItem('jwt_token', newToken);
-    } else {
-      localStorage.removeItem('jwt_token');
-    }
-    setToken(newToken);
-  };
-
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          
-          <Route 
-            path="/*"
-            element={
-              <PrivateRoute token={token}>
-                <Main1 />
-              </PrivateRoute>
-            } 
-          />
-          <Route path="/auth/callback" element={<AuthCallback setAuthToken={setAuthToken} />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
-const PrivateRoute = ({ children, token }) => {
-  return token ? children : <Navigate to="/login" />;
-};
-
-const AuthCallback = ({ setAuthToken }) => {
-    const location = useLocation();
-
-    useEffect(() => {
-        const params = new URLSearchParams(location.search);
-        const token = params.get('token');
-
-        if (token) {
-            setAuthToken(token);
-        }
-    }, [location, setAuthToken]);
-
-    return <Navigate to="/" />;
-};
 
 export default App;
